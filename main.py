50ee2053935c44b52613bf1a68c0994fb0e3bcea
Wiktor Wolak <wwolak@wwolak.pl>
2015-04-14 18:01:55 +0200

    Render a batch in MyWindow.on_draw

--- main.py
+++ main.py
@@ -3,15 +3,25 @@ from pyglet.window import key
 
 
 class MyWindow(pyglet.window.Window):
+    def __init__(self, batch=None):
+        super(MyWindow, self).__init__()
+        self.batch = batch
+
+
     def on_key_press(self, symbol, modifiers):
         print "on_key_press", symbol, modifiers
+
+
+    def on_draw(self):
+        self.batch.draw()
 
 
 class App(object):
     def __init__(self):
         self.app = pyglet.app
         self.batch = pyglet.graphics.Batch()
-        self.window = MyWindow()
+        self.window = MyWindow(batch=self.batch)
+
 
     def run(self):
         self.app.run()
